4694ec49705bca7aafdd26bbfc06ed286d317af1
tapash22 <tapasp263@gmail..com>
2024-01-08 12:07:15 +0600

    add video and other content into home page

---
+++ src/components/videobox/VideoPlayer.vue
@@ -0,0 +1,28 @@
+<!-- VideoPlayer.vue -->
+<template>
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        :src="videoUrl"
+        frameborder="0"
+        allowfullscreen
+      ></iframe>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      videoUrl: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add your component styles here */
+  </style>
+  
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,17 @@
   <div class="home -mt-10">
     <div class="home-header home-banner">
       <div class="banner-block">
-        <span
-          class="home-banner-title user-select-none"
-        >
+        <span class="home-banner-title user-select-none">
           We provide the most advance digital solution
         </span>
-        <p
-          class="home-banner-subtitle user-select-none"
-        >
+        <p class="home-banner-subtitle user-select-none">
           Leading Software Development Studio & Agency
         </p>
         <div class="home-banner-icon-section">
-          <div
-            class="home-banner-icon-block"
-          >
-            <i
-              class="fa fa-play home-banner-icon"
-              aria-hidden="true"
-            ></i>
+          <div class="home-banner-icon-block" @click="showView">
+            <i class="fa fa-play home-banner-icon" aria-hidden="true"></i>
           </div>
-          <p
-            class="home-banner-icon-text user-select-none"
-          >
+          <p class="home-banner-icon-text user-select-none">
             Watch Video
           </p>
         </div>
@@ -36,14 +25,10 @@
         <span class="text-sm font-normal tracking-wide flex justify-start">
           WHO WE ARE
         </span>
-        <p
-          class="text-5xl font-bold tracking-normal py-3 flex justify-center leading-none"
-        >
+        <p class="text-5xl font-bold tracking-normal py-3 flex justify-center leading-none">
           We run all kinds of software services that vow your success
         </p>
-        <p
-          class="text-sm font-medium tracking-normal text-justify break-words leading-7 py-2"
-        >
+        <p class="text-sm font-medium tracking-normal text-justify break-words leading-7 py-2">
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos modi
           tenetur, amet eveniet a placeat voluptatum natus doloremque cumque
           cupiditate adipisci officia alias, illo voluptate unde veniam maxime
@@ -52,36 +37,23 @@
 
         <div class="px-3 py-5 bg-gray-300 rounded-lg flex justify-center my-4">
           <!-- <span class="text-3xl font-bold text-indigo-950 px-2">	&#34;</span> -->
-          <i
-            class="fa fa-share text-3xl font-bold text-indigo-950 px-2 flex items-center"
-            aria-hidden="true"
-          ></i>
-          <p
-            class="text-lg font-bold px-2 tracking-wide text-center text-indigo-950"
-          >
+          <i class="fa fa-share text-3xl font-bold text-indigo-950 px-2 flex items-center" aria-hidden="true"></i>
+          <p class="text-lg font-bold px-2 tracking-wide text-center text-indigo-950">
             Accelerate innovation with world-class tech teams We’ll match you to
             an entire remote.
           </p>
         </div>
         <!-- last part -->
         <div class="w-full my-3 px-3 flex justify-between gap-5">
-          <div
-            class="block bg-gray-200 rounded-lg py-3 px-3"
-            v-for="intro in introduce"
-            :key="intro.id"
-          >
+          <div class="block bg-gray-200 rounded-lg py-3 px-3" v-for="intro in introduce" :key="intro.id">
             <div class="flex justify-center">
-              <i
-                class="fa fa-arrow-right text-sm w-6 h-6 mx-1 bg-indigo-950 rounded-full text-white flex justify-center items-center"
-                aria-hidden="true"
-              ></i>
+              <i class="fa fa-arrow-right text-sm w-6 h-6 mx-1 bg-indigo-950 rounded-full text-white flex justify-center items-center"
+                aria-hidden="true"></i>
               <span class="tracking-wide text-black text-lg font-bold">
                 {{ intro.name }}
               </span>
             </div>
-            <p
-              class="text-sm font-normal text-indigo-950 flex justify-center text-justify py-2"
-            >
+            <p class="text-sm font-normal text-indigo-950 flex justify-center text-justify py-2">
               {{ intro.details }}
             </p>
           </div>
@@ -92,20 +64,12 @@
 
       <!-- right side -->
       <div class="w-1/2 h-auto flex justify-center items-end">
-        <div
-          class="w-full h-2/3 px-2 relative bg-gray-300 opacity-90 rounded-xl"
-        >
+        <div class="w-full h-2/3 px-2 relative bg-gray-300 opacity-90 rounded-xl">
           <div class="absolute right-0 bottom-4">
-            <img
-              src="../assets/about-big.jpg"
-              class="object-cover w-full h-full rounded-lg"
-            />
+            <img src="../assets/about-big.jpg" class="object-cover w-full h-full rounded-lg" />
           </div>
           <div class="absolute bottom-0 -left-4">
-            <img
-              src="../assets/about-sm.jpg"
-              class="object-cover w-full h-auto rounded-lg"
-            />
+            <img src="../assets/about-sm.jpg" class="object-cover w-full h-auto rounded-lg" />
           </div>
         </div>
       </div>
@@ -149,20 +113,12 @@
     <div class="flex section px-5">
       <div class="w-1/2 h-auto block px-3 py-3">
         <h1
-          class="font-bold tracking-wide flex justify-start px-2 text-3xl border-l-4 border-indigo-500 mr-36 mb-5 user-select-none"
-        >
+          class="font-bold tracking-wide flex justify-start px-2 text-3xl border-l-4 border-indigo-500 mr-36 mb-5 user-select-none">
           To make requests for further information, contact us
         </h1>
-        <div
-          class="flex w-full my-2 py-3 px-2"
-          v-for="info in information"
-          :key="info.id"
-        >
+        <div class="flex w-full my-2 py-3 px-2" v-for="info in information" :key="info.id">
           <div class="w-1/4 h-auto flex justify-center items-center">
-            <i
-              :class="info.icon"
-              class="text-6xl fonr-bold text-indigo-950"
-            ></i>
+            <i :class="info.icon" class="text-6xl fonr-bold text-indigo-950"></i>
           </div>
           <div class="w-2/4 block px-1">
             <h3 class="font-bold tracking-normal flex justify-start text-2xl">
@@ -174,52 +130,38 @@
           </div>
         </div>
       </div>
-      <div
-        class="message-section"
-      >
+      <div class="message-section">
         <form class="submit-form" @submit="mailSubmit">
           <span class="form-title">LEAVE US MASSAGE</span>
           <h1 class="form-subtitle">How May We Help You!</h1>
           <div class="block">
             <label class="form-label">Name</label>
-            <input
-              type="text"
-              v-model="mailForm.name"
-              class="form-input"
-              placeholder="Full Name"
-            />
+            <input type="text" v-model="mailForm.name" class="form-input" placeholder="Full Name" />
           </div>
           <div class="block">
             <label class="form-label">Email</label>
-            <input
-              type="text"
-              v-model="mailForm.email"
-              class="form-input"
-              placeholder="Email"
-            />
+            <input type="text" v-model="mailForm.email" class="form-input" placeholder="Email" />
           </div>
           <div class="block">
             <label class="form-label">Subject</label>
-            <input
-              type="text"
-              v-model="mailForm.subject"
-              class="form-input"
-              placeholder="Subject"
-            />
+            <input type="text" v-model="mailForm.subject" class="form-input" placeholder="Subject" />
           </div>
           <div class="block">
             <label class="form-label">Message</label>
-            <textarea
-              v-model="mailForm.message"
-              class="form-textarea"
-              placeholder="Write A Message"
-            >
+            <textarea v-model="mailForm.message" class="form-textarea" placeholder="Write A Message">
             </textarea>
           </div>
           <div class="w-full py-2 block">
             <button class="form-button" type="submit">Send Message</button>
           </div>
         </form>
+      </div>
+    </div>
+
+    <div class="absolute left-1/4 top-1/3 bg-red-700 shadow-xl shadow-red-500 flex justify-center items-center" v-if="show">
+      <VideoPlayer :videoUrl="youtubeVideoUrl" />
+      <div class="absolute -right-3 -top-3 w-10 h-10 flex justify-center align-middle bg-white rounded-xl text-red-900 cursor-pointer font-bold text-2xl" @click="hideView">
+        X
       </div>
     </div>
     <!-- contact block end -->
@@ -230,6 +172,7 @@
 import SwiperView from "@/components/swiper/SwiperView.vue";
 import PartnerSwiper from "@/components/swiper/PartnerSwiper.vue";
 import ProductCard from '@/components/card/ProductCard';
+import VideoPlayer from '@/components/videobox/VideoPlayer'
 // import { introduce,information,cardView } from '../jsonStore/store';
 
 export default {
@@ -237,6 +180,8 @@ export default {
 
   data() {
     return {
+      show:false,
+      youtubeVideoUrl: "https://www.youtube.com/embed/Wy9q22isx3U",
       mailForm: {
         name: "",
         email: "",
@@ -310,18 +255,28 @@ export default {
   components: {
     SwiperView,
     PartnerSwiper,
-    ProductCard
+    ProductCard,
+    VideoPlayer
   },
 
-  mounted(){
-        window.scrollTo(0,0)
-    },
+  mounted() {
+    window.scrollTo(0, 0)
+  },
 
   methods: {
     mailSubmit(e) {
       e.preventDefault();
       console.log(this.mailForm);
     },
+
+    showView(){
+      this.show = true;
+      console.log("click action")
+    },
+    hideView(){
+      this.show = false,
+      console.log("hide view");
+    }
   },
 };
 </script>
