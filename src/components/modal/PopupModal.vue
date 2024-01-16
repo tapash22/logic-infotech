2af57c6908a51248574b7d347ff9feb49cbf9d25
tapash22 <tapasp263@gmail.com>
2024-01-16 18:43:38 +0600

    final touch done for responsive

--- src/components/modal/PopupModal.vue
+++ src/components/modal/PopupModal.vue
@@ -1,11 +1,11 @@
 <template>
     <div
-        class="absolute w-full h-auto left-0 top-40 flex justify-center "
+        class="popup-wrapper"
       >
         <div
-          class="w-1/2 h-auto py-5 px-5 bg-white rounded-lg block shadow-xl shadow-gray-400"
+          class="popup-wrapper-block"
         >
-          <div class="product-icon-block py-3">
+          <div class="product-icon-block popup-wrapper-block-image">
             <img :src="popupview.image" />
           </div>
           <p class="product-type">
@@ -14,27 +14,27 @@
           <p class="product-details leading-7">
             {{ popupview.details }}
           </p>
-          <p class="text-sm font-medium tracking-wide text-justify">
+          <p class="popup-wrapper-block-text">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio,
             quam autem facilis sunt mollitia accusamus facere laboriosam quos.
             Aperiam voluptate maxime minus, et aut magnam expedita qui. Tempore,
             cupiditate atque!
           </p>
-          <router-link to="/contact" class=" flex justify-start gap-2 py-3 cursor-pointer">
-            <span class="text-lg font-bold text-indigo-950 tracking-wide underline underline-offset-4">Contact with us</span> 
+          <router-link to="/contact" class="popup-wrapper-block-link">
+            <span class="popup-wrapper-block-link-text">Contact with us</span> 
           </router-link>
           <div class=" block">
-            <ul class="list-none flex gap-2">
-              <li v-for="sl in socialLinks" :key="sl.id" class="w-10 h-10 rounded-full bg-indigo-950 opacity-80 hover:opacity-100 transition-all delay-100 flex justify-center items-center">
+            <ul class="popup-wrapper-block-social-block-ul">
+              <li v-for="sl in socialLinks" :key="sl.id" class="popup-wrapper-block-social-block-li">
                 <a :href="sl.link" class="no-underline">
-                  <i :class="sl.icon" class="text-2xl font-sm text-white"></i>
+                  <i :class="sl.icon" class="popup-wrapper-block-social-block-li-icon"></i>
                 </a>
               </li>
             </ul>
           </div>
         </div>
-        <div class=" flex justify-center items-center -mt-2 -ml-5 w-10 h-10 rounded-full bg-white ring-2 ring-gray-400 cursor-pointer" >
-          <i class="fa fa-times fa-1x font-normal text-2xl text-gray-500" @click="closeView"></i>
+        <div class="popup-wrapper-block-close" >
+          <i class="fa fa-times " @click="closeView"></i>
         </div>
       </div>
 </template>
